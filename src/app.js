b4b833700351f39c86cd83ff7bfd1c5965c8dd18
Victor Brandao <victorbrandao256@gmail.com>
2022-05-26 14:41:56 -0300

    minor changes only some diferent lines

--- src/app.js
+++ src/app.js
@@ -1,8 +1,9 @@
 const express = require('express');
+
 const router = require('./routes');
+const db = require('./database');
 const handleError = require('./middleware/handleError');
 
-const db = require('./database');
 
 const app = express();
 
@@ -11,10 +12,11 @@ db.hasConnection();
 const port = 3000;
 
 app.use(express.json());
+app.use(handleError);
 app.use(express.urlencoded({ extended: true }));
 
+
 app.use(router);
-app.use(handleError);
 
 app.listen(port, () => {
     console.log(`Servidor executando na porta: ${port}`);
